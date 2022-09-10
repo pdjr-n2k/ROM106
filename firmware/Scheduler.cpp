6be661f94c65227ebcfeeffcf5416027f9a7e0a2
Paul Reeve <preeve@pdjr.eu>
2022-09-10 22:41:00 +0200

    Getting going

--- firmware/Scheduler.cpp
+++ firmware/Scheduler.cpp
@@ -1,17 +1,49 @@
 #include "Arduino.h"
 #include "Scheduler.h"
 
-Scheduler::Scheduler(unsigned long millis) {
-    this->loopInterval = millis;
+/**********************************************************************
+ * Create a new Scheduler with the process interval specified by
+ * <loopInterval>.  The specified interval is the frequency at which
+ * the scheduler will check to see if a callback should be executed, so
+ * its best if this is frequent.
+ */
+ 
+Scheduler::Scheduler(unsigned long loopInterval) {
+    this->loopInterval = loopInterval;
 }
 
-bool Scheduler::schedule(unsigned long when, void (*func)(), unsigned int opcode) {
+/**********************************************************************
+ * This function must be called from the main loop(). It will execute
+ * any scheduled callback function and then delete it from the
+ * collection of scheduled callback functions (unless the callback was
+ * scheduled with a repeat flag in which cast the callback will be
+ * re-scheduled).
+ */
+
+void Scheduler::loop() {
+    static unsigned long next = 0UL;
+    unsigned long now = millis();
+
+    if (now > next) {
+        for (unsigned int i = 0; i < 10; i++) {
+            if (this->scheduledEvents[i].next >= now) {
+                this->scheduledEvents[i].func(this->scheduledEvents[i].opcode);
+                this->scheduledEvents[i].next = (this->scheduledEvents[i].repeat)?(now + this->scheduledEvents[i].interval):0UL;
+            }
+        }
+        next = (now + this->loopInterval);
+    } 
+}
+
+bool Scheduler::schedule(void (*func)(), unsigned long interval, bool repeat, unsigned int opcode) {
     bool retval = false;
 
     for (unsigned int i = 0; i < 10; i++) {
-        if (this->scheduledEvents[i].when == 0UL) {
-            this->scheduledEvents[i].when = (millis() + when);
+        if (this->scheduledEvents[i].interval == 0UL) {
             this->scheduledEvents[i].func = func;
+            this->scheduledEvents[i].interval = interval;
+            this->scheduledEvents[i].next = (millis() + interval);
+            this->scheduledEvents[i].repeat = repeat;
             this->scheduledEvents[i].opcode = opcode;
             retval = true;
             break;
@@ -20,17 +52,3 @@ bool Scheduler::schedule(unsigned long when, void (*func)(), unsigned int opcode
     return(retval);
 }
 
-void Scheduler::loop() {
-    static unsigned long next = 0UL;
-    unsigned long now = millis();
-
-    if (now > next) {
-        for (unsigned int i = 0; i < 10; i++) {
-            if (this->scheduledEvents[i].when >= now) {
-                this->scheduledEvents[i].func(this->scheduledEvents[i].opcode);
-                this->scheduledEvents[i].when = 0UL;
-            }
-        }
-        next = (now + this->loopInterval);
-    } 
-}
--- firmware/Scheduler.h
+++ firmware/Scheduler.h
@@ -1,18 +1,42 @@
+/**********************************************************************
+ * Scheduler - callback scheduler.
+ * 2022 (c) Paul Reeve.
+ * 
+ * Example
+ * 
+ * #define LOOP_INTERVAL 100UL
+ * 
+ * Scheduler myScheduler(LOOP_INTERVAL);
+ * 
+ * void setup() {
+ *   myScheduler.schedule(2000UL, myCallbackFunction);
+ * }
+ * 
+ * void loop() {
+ *   mySchedular.loop();
+ * }
+ * 
+ * void myCallbackFunction() {
+ *   Serial.println("Hello world");
+ * }
+ * 
+ */
+
 #ifndef SCHEDULER_H
 #define SCHEDULER_H
 
 class Scheduler {
 
 public:
-    Scheduler(unsigned long processInterval);
-    bool schedule(unsigned long when, void (*func)(), unsigned int opcode);
+    Scheduler(unsigned long loopInterval);
+    bool schedule(void (*func)(), unsigned long interval, bool repeat, unsigned int opcode);
     void loop();
 
 protected:
 
 private:
-    struct schduledEvent { unsigned long when, void (*func)(), unsigned int opcode = 0 };
-    scheduledEvent[] scheduledEvents = { {0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0},{0UL,0,0} };
+    struct schduledEvent { void (*func)(), unsigned long interval, unsigned long next, bool repeat = false, unsigned int opcode = 0 };
+    scheduledEvent[] scheduledEvents = { {0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0},{0,0UL,0UL,false,0} };
     unsigned long loopInterval = 100UL;
 
 }
