f90ce4d22f977790f0d86770f2c251476c7ffdff
Paul Reeve <preeve@pdjr.eu>
2023-01-29 10:21:18 +0100

    Update defines.h

--- firmware/ROM106.1/defines.h
+++ firmware/ROM106.1/defines.h
@@ -18,16 +18,13 @@
 #define GPIO_RELAY_RST GPIO_D9
 #define GPIO_RELAY_CONTROL_OUTPUT_PINS { GPIO_RELAY1_ENABLE, GPIO_RELAY2_ENABLE, GPIO_RELAY3_ENABLE, GPIO_RELAY4_ENABLE, GPIO_RELAY5_ENABLE, GPIO_RELAY6_ENABLE, GPIO_RELAY_SET, GPIO_RELAY_RST }
 
-/**
- * @brief Device information overrides for ROM106.
+/**********************************************************************
+ * @brief Device and product information overrides for ROM106.
  */
 #define DEVICE_CLASS 30                 // Electrical Distribution
 #define DEVICE_FUNCTION 140             // Load Controller
 #define DEVICE_UNIQUE_NUMBER 849        // Bump me?
 
-/**
- * @brief Product information overrided for ROM106.
- */
 #define PRODUCT_CODE 002
 #define PRODUCT_FIRMWARE_VERSION "1.1.0 (Jun 2022)"
 #define PRODUCT_LEN 1
@@ -35,18 +32,18 @@
 #define PRODUCT_TYPE "ROM106"           // The product name?
 #define PRODUCT_VERSION "1.0 (Mar 2022)"
 
-/**
+/**********************************************************************
  * @brief PGN overrides for ROM106.
  * */
 void handlePGN127502(const tN2kMsg &n2kMsg);
 
-#define NMEA_TRANSMIT_MESSAGE_PGNS { 127501L, 0 }
-#define NMEA_PGN_HANDLERS  { { 127502L, handlePGN127502 }, { 0L, 0 } }
+#define NMEA_TRANSMITTED_PGNS { 127501L, 0 }
+#define NMEA_RECEIVED_PGNS  { { 127502L, handlePGN127502 }, { 0L, 0 } }
 
 /**********************************************************************
  * Module configuration stuff.
  */
-#define MODULE_CONFIGURATION_SIZE 3
+#define MODULE_CONFIGURATION_SIZE 4
 
 #define MODULE_CONFIGURATION_INSTANCE_INDEX 1
 #define MODULE_CONFIGURATION_PGN127501_TRANSMIT_PERIOD_INDEX 2
